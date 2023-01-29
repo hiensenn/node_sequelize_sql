70b4540a038d353e0739f7782842e7319ddae793
hiensenn <hiensen.cdl@gmail.com>
2023-01-28 21:42:27 -0300

    Atualizando os dados

--- index.js
+++ index.js
@@ -52,6 +52,44 @@ app.get('/users/:id', async (req, res) => {
     res.render('userview', {user})
 })
 
+app.post('/users/delete/:id', async(req, res) => {
+    const id = req.params.id
+
+    await User.destroy({where: {id:id}})
+    res.redirect('/')
+})
+
+app.get('/users/edit/:id', async(req, res) => {
+    const id = req.params.id
+
+    const user = await User.findOne({raw: true, where: {id:id}})
+    res.render('useredit', {user})
+})
+
+app.post('/users/update', async(req, res) => {
+    const id = req.body.id
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+
+    if(newsletter === 'on'){
+        newsletter = true
+    } else{
+        newsletter = false
+    }
+
+    const userData = {
+        id, 
+        name,
+        occupation,
+        newsletter
+    }
+
+    await User.update(userData, {where: {id:id}})
+    res.redirect('/')
+})
+
+
 //criando página principal
 app.get('/', async (req, res) => {
 
